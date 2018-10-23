47e4dfd34eec3270ec6a28724f042bb558e47c2c
lyj <1017065069@qq.com>
2018-10-23 11:18:17 +0800

    创建访谈mapper

--- src/main/java/cn/xzt/interview/mapper/InterviewMapper.java
+++ src/main/java/cn/xzt/interview/mapper/InterviewMapper.java
@@ -1,6 +1,7 @@
 package cn.xzt.interview.mapper;
 
 import cn.xzt.interview.domain.Interview;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -10,6 +11,8 @@ import java.util.List;
  * @Date: 2018/10/23 10:05
  */
 public interface InterviewMapper {
-    List<Interview> findAll();
-    Interview selectByPrimaryKey();
+    List<Interview> findAll(@Param("status") String status);
+    Interview selectByPrimaryKey(@Param("interviewId") String interviewId);
+    void insertInterview(Interview interview);
+    void updateInterview(Interview interview);
 }

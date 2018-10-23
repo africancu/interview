d7f6848f07a68f964429adc0c346af81067bfb06
非洲铜 <africancu@126.com>
2018-10-23 09:02:22 +0800

    新增dev分支

--- src/main/java/cn/xzt/interview/controller/UserController.java
+++ src/main/java/cn/xzt/interview/controller/UserController.java
@@ -1,9 +1,17 @@
 package cn.xzt.interview.controller;
 
+import cn.xzt.interview.common.utils.R;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @Description:
  * @Auther: 周明军
  * @Date: 2018/10/22 18:04
  */
+@RestController
 public class UserController {
+
+    public R login(String username, String password){
+        return null;
+    }
 }

bd680f707e496a2db6e2bbcb1ea6b95b15dd8498
lyj <1017065069@qq.com>
2018-10-23 10:12:28 +0800

    创建访谈mapper

---
+++ src/main/java/cn/xzt/interview/mapper/InterviewMapper.java
@@ -0,0 +1,15 @@
+package cn.xzt.interview.mapper;
+
+import cn.xzt.interview.domain.Interview;
+
+import java.util.List;
+
+/**
+ * @Description:
+ * @Auther: lyj
+ * @Date: 2018/10/23 10:05
+ */
+public interface InterviewMapper {
+    List<Interview> findAll();
+
+}

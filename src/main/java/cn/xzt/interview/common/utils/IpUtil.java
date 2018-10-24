029c4055cdc7b6479b3e2b2dbb3f24b8978e65e8
QiuJay <89549737@qq.com>
2018-10-24 15:04:54 +0800

    IP 相关工具类

---
+++ src/main/java/cn/xzt/interview/common/utils/IpUtil.java
@@ -0,0 +1,34 @@
+package cn.xzt.interview.common.utils;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by Jay on 2018/10/24 15:02.
+ *  IP 相关工具类
+ * @author Jay
+ */
+public class IpUtil {
+
+    /**
+     * @return 获取客户端IP地址
+     */
+    public static String getIpAddr(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        // 多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
+        if (ip != null && ip.length() > 15) {
+            if (ip.indexOf(",") > 0) {
+                ip = ip.substring(0, ip.indexOf(","));
+            }
+        }
+        return ip;
+    }
+}

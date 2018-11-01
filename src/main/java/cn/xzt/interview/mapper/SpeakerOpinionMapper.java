87ce4ef5bed64caada91d99ffba3e91e1c4f48d3
lyj <1017065069@qq.com>
2018-11-01 18:12:43 +0800

    新增 文字实录接口

---
+++ src/main/java/cn/xzt/interview/mapper/SpeakerOpinionMapper.java
@@ -0,0 +1,12 @@
+package cn.xzt.interview.mapper;
+
+import cn.xzt.interview.domain.SpeakerOpinion;
+
+/**
+ * @Description:
+ * @Auther: lyj
+ * @Date: 2018/11/1 17:33
+ */
+public interface SpeakerOpinionMapper {
+    void addspeakerOpinion(SpeakerOpinion speakerOpinion);
+}
---
+++ src/main/java/cn/xzt/interview/service/impl/SpeakerOpinionServiceImpl.java
@@ -0,0 +1,26 @@
+package cn.xzt.interview.service.impl;
+
+import cn.xzt.interview.domain.SpeakerOpinion;
+import cn.xzt.interview.mapper.SpeakerOpinionMapper;
+import cn.xzt.interview.service.SpeakerOpinionService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Description:
+ * @Auther: lyj
+ * @Date: 2018/11/1 17:45
+ */
+
+@Service(value = "SpeakerOpinionService")
+@Slf4j
+public class SpeakerOpinionServiceImpl implements SpeakerOpinionService {
+    @Autowired
+    private SpeakerOpinionMapper speakerOpinionMapper;
+
+    @Override
+    public void addspeakerOpinion(SpeakerOpinion speakerOpinion) {
+        speakerOpinionMapper.addspeakerOpinion(speakerOpinion);
+    }
+}
